a1c62a003751cf2210f7f1f8c7f2707dace55a98
utyosu <utyosu@gmail.com>
2018-05-05 19:26:10 +0900

    @1or3募集にもっと対応 close #20

--- bin/discord/lib.rb
+++ bin/discord/lib.rb
@@ -11,8 +11,8 @@ def to_safe(str)
 end
 
 def extraction_recruit_user_count(str)
-  tmp = to_safe(str).match(/@\d+(or|か)(\d)/)
-  return tmp[2].to_i if tmp.present?
+  tmp = to_safe(str).match(/@\d[^\d]+(\d)([^:時]|\Z)/)
+  return tmp[1].to_i if tmp.present?
   tmp = to_safe(str).match(/@(\d+)/)
   return tmp.blank? ? nil : tmp[1].to_i
 end
