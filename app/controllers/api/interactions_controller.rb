e33e1cf6fca3e77d25afad1ba1be55bf0e36b6e9
utyosu <utyosu@gmail.com>
2018-05-11 09:32:18 +0900

    同一キーワードを記憶できるように修正

--- app/controllers/api/interactions_controller.rb
+++ app/controllers/api/interactions_controller.rb
@@ -5,28 +5,22 @@ module Api
     end
 
     def create
-      interaction = Interaction.find_by(keyword: interaction_params[:keyword])
       user = User.find_or_initialize_by(discord_id: params[:interaction][:registered_user_discord_id])
       user.update(name: params[:interaction][:registered_user_name])
-      if interaction.present?
-        interaction.update(interaction_params.merge(user: user))
-      else
-        interaction = Interaction.create!(interaction_params.merge(user: user))
-      end
+      interaction = Interaction.create!(interaction_params.merge(user: user))
       render json: interaction, status: 201
     end
 
     def destroy_by_keyword
-      interaction = Interaction.find_by(keyword: params[:keyword])
-      return head 404 if interaction.blank?
-      interaction.destroy
+      interactions = Interaction.where(keyword: params[:keyword])
+      return head 404 if interactions.blank?
+      interactions.destroy_all
       head 200
     end
 
     def search
-      Interaction.all.sort_by{|i|i.keyword.length}.reverse.each do |interaction|
-        return render json: {response: interaction.response} if params[:keyword].include?(interaction.keyword)
-      end
+      interaction = Interaction.where("keyword like '%#{params[:keyword]}%'").sample
+      return render json: {response: interaction.response} if interaction.present?
       return render json: {}
     end
 
