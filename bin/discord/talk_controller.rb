ac1fc83d593bdc71c8b63a220ac875836c8d5fc8
utyosu <utyosu@gmail.com>
2018-07-07 11:49:59 +0900

    お話機能をより反応するように強化

--- bin/discord/talk_controller.rb
+++ bin/discord/talk_controller.rb
@@ -2,11 +2,12 @@ require_relative 'api'
 module TalkController
   extend self
 
-  TALK_REGEXP = /\A#{ENV['DISCORD_BOT_TALK_WORD']}(.+)/
+  TALK_REGEXP = /\A(.*)#{ENV['DISCORD_BOT_TALK_WORD']}(.*)\Z/
 
   def talk(message_event)
     return if !check_limit(message_event, "play", ENV['DISCORD_BOT_PLAY_LIMIT'] || 10)
-    query = message_event.content.match(TALK_REGEXP)[1]
+    query = message_event.content.match(TALK_REGEXP)[1..-1].join
+    puts query
     response = HTTP.post("https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk", form: {apikey: ENV['DISCORD_BOT_TALK_APIKEY'], query: query})
     return if response.status != 200
     fields = JSON.parse(response.body)
