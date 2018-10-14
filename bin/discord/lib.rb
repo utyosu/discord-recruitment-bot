e71de7958559f547408d55c3c1ef2f43a36770d1
utyosu <utyosu@gmail.com>
2018-10-14 14:06:46 +0900

    メッセージに全角スペースが含まれていても反応するように改善

--- bin/discord/lib.rb
+++ bin/discord/lib.rb
@@ -8,7 +8,7 @@ end
 
 def to_safe(str)
   # <@\d+> is discord mention
-  str.tr('０-９ａ-ｚＡ-Ｚ＠？：', '0-9a-zA-Z@?:').gsub(/<@\d+>/, "")
+  str.tr('０-９ａ-ｚＡ-Ｚ＠？：', '0-9a-zA-Z@?:').gsub(/<@\d+>/, "").gsub(/[[:blank:]]/, " ")
 end
 
 def extraction_recruit_user_count(str)
