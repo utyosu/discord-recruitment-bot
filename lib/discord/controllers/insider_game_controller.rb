57744d926ad102d687cde92d6f524d467641165a
utyosu <utyosu@gmail.com>
2019-07-14 02:11:12 +0900

    インサイダーゲームでマスターがインサイダーに選ばれる不具合を修正

--- lib/discord/controllers/insider_game_controller.rb
+++ lib/discord/controllers/insider_game_controller.rb
@@ -4,9 +4,9 @@ module InsiderGameController
   def insider_game(message_event)
     command, subject = message_event.content.split(/[[:blank:]]/, 2)
     author = message_event.author
-    voice_channel = $bot.servers.map{|server_id, server|
+    voice_channel = $bot.servers.map{ |server_id, server|
       server.voice_channels.find{ |voice_channel|
-        voice_channel.users.any?{|user|
+        voice_channel.users.any?{ |user|
           user.id == author.id
         }
       }
@@ -18,7 +18,12 @@ module InsiderGameController
     end
 
     users = voice_channel.users
-    insider = users.sample
+    insider = users.reject{|user| user.id == author.id}.sample
+
+    if insider.blank?
+      author.pm("インサイダーがいません。")
+      return
+    end
 
     users.each do |user|
       if user.id == insider.id
