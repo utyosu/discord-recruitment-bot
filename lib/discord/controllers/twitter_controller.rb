099d5f89f2aca1944b5d60eaaafacdcf34a6f212
utyosu <utyosu@gmail.com>
2019-07-28 20:45:43 +0900

    Twitterのi18nのtypoを修正

--- lib/discord/controllers/twitter_controller.rb
+++ lib/discord/controllers/twitter_controller.rb
@@ -26,7 +26,7 @@ class TwitterController
 
   def self.recruitment_message(recruitment)
     message = "【#{ENV['DISCORD_BOT_TWITTER_NOTICE_TITLE']}】\n#{recruitment.content} by #{recruitment.author.name} (#{recruitment.reserved}/#{recruitment.capacity})"
-    message += "\n#{I18n.t('twitter:participants')}: #{recruitment.participants[1..-1].map{|a|a.user.name}.join(", ")}" if 0 < recruitment.reserved
+    message += "\n#{I18n.t('twitter.participants')}: #{recruitment.participants[1..-1].map{|a|a.user.name}.join(", ")}" if 0 < recruitment.reserved
     return message
   end
 
