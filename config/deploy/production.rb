de497a1ab62a34603a453ab323128ad739540491
utyosu <utyosu@gmail.com>
2020-01-26 00:07:24 +0900

    masterブランチへのpush時にdeployするワークフローを追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,5 @@
 role :app, %w{ops@gce}
 
-server "gce",
-  user: "ops",
-  roles: %w{app},
-  ssh_options: {
-    user: "ops",
-    keys: %w(/home/ops/.ssh/google_compute_engine),
-    forward_agent: false,
-    auth_methods: %w(publickey)
-  }
+server "gce", user: "ops", roles: %w{app}
 
 set :rails_env, :production
