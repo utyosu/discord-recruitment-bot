33a4c347e4efe5d3f56ff58c1f6cdbe33272294d
utyosu <utyosu@gmail.com>
2020-01-25 15:01:28 +0900

    ヘルプの文言をリソースファイルに移動

--- lib/discord/controllers/help_controller.rb
+++ lib/discord/controllers/help_controller.rb
@@ -2,22 +2,8 @@ module HelpController
   extend self
 
   def help(message_event)
-    ret = []
-
     if $recruitment_channel == message_event.channel
-      ret << "__募集機能__"
-      ret << ""
-      ret << "`@<数字>` - 募集の開始"
-      ret << "`<数字>参加` - 募集に参加"
-      ret << "`<数字>キャンセル` - 募集から参加をキャンセル"
-      ret << "`<数字>しめ` - 募集を終了"
-      ret << "`復活` - 最後に締めた募集を復活"
-      ret << "`案件` - 現在の募集を表示"
-      ret << ""
-    end
-
-    if ret.present?
-      message_event.send_message(ret.join("\n"))
+      message_event.send_message(I18n.t('help.recruitment'))
     end
   end
 end
