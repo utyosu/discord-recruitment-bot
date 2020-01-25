57c883747b18f85a08456557a63c8e5b7bef714a
utyosu <utyosu@gmail.com>
2020-01-25 13:54:51 +0900

    PrivateMessageのときはdisplay_nameが取得できないので更新しないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   def self.get_by_discord_user(discord_user)
     user = User.find_or_initialize_by(discord_id: discord_user.id)
-    user.update(name: discord_user.display_name)
+    display_name = discord_user.try(:display_name)
+    user.update(name: display_name) if display_name.present?
     return user
   end
 end
