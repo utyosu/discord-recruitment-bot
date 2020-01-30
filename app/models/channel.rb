a1db56518aeccc27975047f18a7380762c78cb69
utyosu <utyosu@gmail.com>
2020-01-31 00:47:52 +0900

    Channelモデルの変数重複を解消

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,7 +1,7 @@
 class Channel < ApplicationRecord
-  def self.get_by_discord_channel(channel)
-    channel = Channel.find_or_initialize_by(channel_id: channel.id)
-    channel.update(name: channel.name)
+  def self.get_by_discord_channel(discord_channel)
+    channel = Channel.find_or_initialize_by(channel_id: discord_channel.id)
+    channel.update(name: discord_channel.name)
     return channel
   end
 end
