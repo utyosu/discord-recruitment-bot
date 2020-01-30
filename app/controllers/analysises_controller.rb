e2f0a3c69d64a7ca635e6d50669606aa978a66a4
utyosu <utyosu@gmail.com>
2020-01-31 08:00:55 +0900

    AnalysisesControllerのリファクタリング

--- app/controllers/analysises_controller.rb
+++ app/controllers/analysises_controller.rb
@@ -8,7 +8,7 @@ class AnalysisesController < ApplicationController
   def index
     @start_date = Date.parse(params[:start_date]) rescue Date.today
     @end_date = Date.parse(params[:end_date]) rescue Date.today
-    user_statuses = UserStatus.where(created_at: @start_date.in_time_zone..(@end_date.in_time_zone+60*60*24))
+    user_statuses = UserStatus.where(created_at: @start_date.in_time_zone..@end_date.in_time_zone.since(1.day))
     @channel_use_time_list = {}
     @user_login_time_list = {}
     user_statuses.each do |user_status|
