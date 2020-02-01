0c90449ae96b682ea9bf9b6c62710c95cc78d071
utyosu <utyosu@gmail.com>
2020-02-01 15:51:29 +0900

    インデントの調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,17 +7,17 @@ set :branch, ENV['BRANCH'] || "master"
 
 set :bundle_jobs, 1
 
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-set :pty,             true
-set :use_sudo,        false
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/ops/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_threads, [4, 16]
+set :puma_workers, 0
+set :pty, true
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/ops/#{fetch(:application)}"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
@@ -31,6 +31,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push(
   'public/system',
   'public/uploads'
 )
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
