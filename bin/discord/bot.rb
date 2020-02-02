77aa2f125537c4229600796a23475a32e3e80b11
utyosu <utyosu@gmail.com>
2020-02-02 17:35:51 +0900

    Botクラスをlib以下に移動

--- bin/discord/bot.rb
+++ bin/discord/bot.rb
@@ -1,68 +1,7 @@
+BOT_DAEMONIZE = (ARGV[0] != "nodaemon")
+
 # Load rails framework
 require_relative '../../config/environment'
-
-# Load discord-recruitment-bot scripts
-Dir['lib/discord/**/*.rb'].each {|file| require './' + file }
-
-def inheritance
-  if ARGV[0] == "nodaemon"
-    Object
-  else
-    DaemonSpawn::Base
-  end
-end
-
-class Bot < inheritance
-  def start(args)
-    loop do
-      self.sequence()
-      sleep 60
-    end
-  end
-
-  def sequence
-    bot = Discordrb::Commands::CommandBot.new ({
-      token: Settings.secret.discord.token,
-      client_id: Settings.secret.discord.client_id,
-      prefix:'/',
-      # log_mode: :debug,
-    })
-
-    bot.message do |event|
-      if event.kind_of?(Discordrb::Events::MessageEvent)
-        ActionSelector.get_message(event, bot)
-      end
-    end
-
-    bot.run(true)
-
-    logger = Logger.new STDOUT
-    logger.info I18n.t('bot.analysis_interval', interval: AnalysisController::ANALYSIS_INTERVAL)
-
-    recruitment_channel = Helper.get_channel(bot, Settings.secret.discord.recruitment_channel_id)
-    logger.info I18n.t('bot.recruitment_channel', name: recruitment_channel.try(:name))
-
-    play_channel = Helper.get_channel(bot, Settings.secret.discord.play_channel_id)
-    logger.info I18n.t('bot.play_channel', name: play_channel.try(:name))
-    logger.info I18n.t('bot.use_twitter', bool: TwitterController.ready?)
-
-    loop do
-      RecruitmentController.destroy_expired_recruitment(recruitment_channel)
-      AnalysisController.voice_channels(bot)
-      sleep 10
-    end
-  rescue => e
-    STDERR.puts I18n.t('bot.reboot')
-    STDERR.puts e.full_message
-    Slack::Web::Client.new(token: Settings.secret.slack.access_token).chat_postMessage(
-      channel: Settings.secret.slack.notify_channel,
-      text: "[#{Rails.env}] #{I18n.t('bot.reboot')}\n```#{e.full_message(highlight: false)}```"
-   )
-   bot.stop
-  end
-
-  def stop; end
-end
 
 if ARGV[0] == "nodaemon"
   bot = Bot.new
