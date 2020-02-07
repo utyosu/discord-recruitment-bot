5651f2cb57cfdf29c7e97d4f183107b57ffdff92
utyosu <utyosu@gmail.com>
2020-02-07 16:37:11 +0900

    fix: rubocop Style/MethodCallWithoutArgsParentheses

--- lib/discord/bot.rb
+++ lib/discord/bot.rb
@@ -3,7 +3,7 @@ BOT_DAEMONIZE = false unless defined?(BOT_DAEMONIZE)
 class Bot < BOT_DAEMONIZE ? DaemonSpawn::Base : Object
   def start(_args)
     loop do
-      self.sequence()
+      self.sequence
     end
   end
 
