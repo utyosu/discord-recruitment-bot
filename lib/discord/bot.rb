3db842959fdc7972785716eaf64624c544132bd0
utyosu <utyosu@gmail.com>
2020-02-11 17:20:00 +0900

    botのリファクタリング

--- lib/discord/bot.rb
+++ lib/discord/bot.rb
@@ -15,13 +15,27 @@ class Bot < BOT_DAEMONIZE ? DaemonSpawn::Base : Object
 
     action_selector = ActionSelector.new
     bot.message do |event|
-      if event.is_a?(Discordrb::Events::MessageEvent)
-        action_selector.execute(event)
-      end
+      action_selector.execute(event) if event.is_a?(Discordrb::Events::MessageEvent)
     end
 
     bot.run(true)
+    logging_startup(bot)
+    timers = Timers::Group.new
+    recruitment_base = RecruitmentBase.new
+    recruitment_channel = Helper.get_channel(bot, Settings.secret.discord.recruitment_channel_id)
+    analysis_controller = AnalysisController.new
+    timers.every(1.minute) do
+      recruitment_base.destroy_expired_recruitment(recruitment_channel)
+      analysis_controller.voice_channels(bot)
+    end
+    loop { timers.wait }
+  rescue StandardError => e
+    logging_error(e)
+    bot.stop
+    sleep 60
+  end
 
+  def logging_startup(bot)
     logger = Logger.new(STDOUT)
     logger.info I18n.t('bot.analysis_interval', interval: AnalysisController::ANALYSIS_INTERVAL)
 
@@ -31,24 +45,16 @@ class Bot < BOT_DAEMONIZE ? DaemonSpawn::Base : Object
     play_channel = Helper.get_channel(bot, Settings.secret.discord.play_channel_id)
     logger.info I18n.t('bot.play_channel', name: play_channel.try(:name))
     logger.info I18n.t('bot.use_twitter', bool: TwitterController.new.ready?)
+  end
 
-    timers = Timers::Group.new
-    recruitment_base = RecruitmentBase.new
-    analysis_controller = AnalysisController.new
-    timers.every(1.minute) do
-      recruitment_base.destroy_expired_recruitment(recruitment_channel)
-      analysis_controller.voice_channels(bot)
-    end
-    loop { timers.wait }
-  rescue StandardError => e
+  def logging_error(error)
+    logger = Logger.new(STDOUT)
     logger.error I18n.t('bot.reboot')
-    logger.error e.full_message
+    logger.error error.full_message
     Slack::Web::Client.new(token: Settings.secret.slack.access_token).chat_postMessage(
       channel: Settings.secret.slack.notify_channel,
-      text: "[#{Rails.env}] #{I18n.t('bot.reboot')}\n```#{e.full_message(highlight: false)}```",
+      text: "[#{Rails.env}] #{I18n.t('bot.reboot')}\n```#{error.full_message(highlight: false)}```",
     )
-    bot.stop
-    sleep 60
   end
 
   def stop; end
