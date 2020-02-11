27581582f094f771cec387b4abc1f694a4006a3f
utyosu <utyosu@gmail.com>
2020-02-11 17:04:21 +0900

    WeatherActionのリファクタリング

--- lib/discord/actions/weather_action.rb
+++ lib/discord/actions/weather_action.rb
@@ -2,51 +2,73 @@ class WeatherAction
   DIFF_TO_ABSOLUTE_ZERO = 273.15
 
   def execute?(message_event)
-    message_event.play? && message_event.match_any_keywords?(Settings.keyword.weather)
+    message_event.play? &&
+      message_event.match_any_keywords?(Settings.keyword.weather) &&
+      Settings.secret.yahoo_geocode_api.appid.present? &&
+      Settings.secret.open_weather_map.appid.present?
   end
 
   def execute(message_event)
-    if Settings.secret.yahoo_geocode_api.appid.blank? || Settings.secret.open_weather_map.appid.blank?
-      Logger.new(STDOUT).puts "Need settings: yahoo_geocode_api.appid and open_weather_map.appid"
-      return
-    end
-
     Activity.add(message_event.author, :weather)
 
-    qurty_string = message_event.content.gsub(Regexp.new(Settings.keyword.weather.join('|')), '')
+    city_word = message_event.content.gsub(Regexp.new(Settings.keyword.weather.join('|')), '')
+
+    city = city_from_word(city_word)
+    return if city.blank?
+
+    weather = weather_from_position(city[:lon], city[:lat])
+    message_event.channel.send_embed do |embed|
+      embed.title = "#{city[:name]}#{I18n.t('weather.display')}"
+      embed.description = I18n.t('weather.format', weather_to_hash(weather))
+      embed.color = 0x5882FA
+    end
+  end
+
+  def weather_to_hash(weather)
+    {
+      weather: Settings.weather.patterns.map(&:split).to_h[weather['weather'].first['id'].to_s],
+      temp: format("%<temp>.1f", temp: (weather['main']['temp'].to_f - DIFF_TO_ABSOLUTE_ZERO)),
+      temp_max: format("%<temp_max>.1f", temp_max: (weather['main']['temp_max'].to_f - DIFF_TO_ABSOLUTE_ZERO)),
+      temp_min: format("%<temp_min>.1f", temp_min: (weather['main']['temp_min'].to_f - DIFF_TO_ABSOLUTE_ZERO)),
+      humidity: weather['main']['humidity'],
+      pressure: weather['main']['pressure'],
+      speed: weather['wind']['speed'],
+      sunrise: Time.zone.at(weather['sys']['sunrise']).strftime('%H:%M'),
+      sunset: Time.zone.at(weather['sys']['sunset']).strftime('%H:%M'),
+    }
+  end
+
+  def city_from_word(word)
     geocode_response = HTTP.get(
       "https://map.yahooapis.jp/geocode/V1/geoCoder",
       params: {
         output: "json",
         appid: Settings.secret.yahoo_geocode_api.appid,
-        query: qurty_string,
+        query: word,
         al: 3,
         ar: "le",
       }
     )
-    return if geocode_response.status != 200 || JSON.parse(geocode_response)['Feature'].blank?
+    return nil if geocode_response.status != 200 || JSON.parse(geocode_response)['Feature'].blank?
     city = JSON.parse(geocode_response)['Feature'].sample
     lon, lat = city['Geometry']['Coordinates'].split(",")
-    weather_response = HTTP.get("https://api.openweathermap.org/data/2.5/weather", params: { appid: Settings.secret.open_weather_map.appid, lat: lat.to_f, lon: lon.to_f })
+    return {
+      name: city['Name'],
+      lon: lon,
+      lat: lat,
+    }
+  end
+
+  def weather_from_position(lon, lat)
+    weather_response = HTTP.get(
+      "https://api.openweathermap.org/data/2.5/weather",
+      params: {
+        appid: Settings.secret.open_weather_map.appid,
+        lat: lat.to_f,
+        lon: lon.to_f,
+      }
+    )
     return if weather_response.status != 200
-    weather = JSON.parse(weather_response)
-    temp = format("%<temp>.1f", temp: (weather['main']['temp'].to_f - DIFF_TO_ABSOLUTE_ZERO))
-    temp_max = format("%<temp_max>.1f", temp_max: (weather['main']['temp_max'].to_f - DIFF_TO_ABSOLUTE_ZERO))
-    temp_min = format("%<temp_min>.1f", temp_min: (weather['main']['temp_min'].to_f - DIFF_TO_ABSOLUTE_ZERO))
-    weather_patterns = Settings.weather.patterns.map(&:split).to_h
-    weather_string = weather_patterns[weather['weather'].first['id'].to_s]
-    res = []
-    res << I18n.t('weather.title.weather', weather: weather_string)
-    res << I18n.t('weather.title.temp', temp: temp, temp_min: temp_min, temp_max: temp_max)
-    res << I18n.t('weather.title.humidity', humidity: weather['main']['humidity'])
-    res << I18n.t('weather.title.pressure', pressure: weather['main']['pressure'])
-    res << I18n.t('weather.title.speed', speed: weather['wind']['speed'])
-    res << I18n.t('weather.title.sunrise', sunrise: Time.zone.at(weather['sys']['sunrise']).strftime('%H:%M'))
-    res << I18n.t('weather.title.sunset', sunset: Time.zone.at(weather['sys']['sunset']).strftime('%H:%M'))
-    message_event.channel.send_embed do |embed|
-      embed.title = "#{city['Name']}#{I18n.t('weather.display')}"
-      embed.description = res.join("\n")
-      embed.color = 0x5882FA
-    end
+    return JSON.parse(weather_response)
   end
 end
