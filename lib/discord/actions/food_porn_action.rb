5a33e1d6893690450604ab907fca63003cd52f06
utyosu <utyosu@gmail.com>
2020-02-11 17:21:13 +0900

    food pornのリファクタリング

--- lib/discord/actions/food_porn_action.rb
+++ lib/discord/actions/food_porn_action.rb
@@ -5,25 +5,7 @@ class FoodPornAction
 
   def execute(message_event)
     Activity.add(message_event.author, :food_porn)
-
-    word =
-      if rand(8192) == 0
-        Settings.food_porn.rare_word.sample
-      else
-        "#{Settings.food_porn.base_word} #{Settings.food_porn.words.sample}"
-      end
-
-    http = HTTP.get(
-      "https://www.googleapis.com/customsearch/v1",
-      params: {
-        key: Settings.secret.google_search_api.key,
-        cx: Settings.secret.google_search_api.cx,
-        q: word,
-        num: 1,
-        start: rand(1..10),
-        searchType: "image",
-      },
-    )
+    http = request_to_customsearch
     if http.status != 200
       message_event.send_message(I18n.t('food_porn.error'))
       return
@@ -36,5 +18,27 @@ class FoodPornAction
     end
     message_event.send_file(File.open(path, "r"))
     File.delete(path)
+  end
+
+  def search_word
+    if rand(8192) == 0
+      Settings.food_porn.rare_word.sample
+    else
+      "#{Settings.food_porn.base_word} #{Settings.food_porn.words.sample}"
+    end
+  end
+
+  def request_to_customsearch
+    HTTP.get(
+      "https://www.googleapis.com/customsearch/v1",
+      params: {
+        key: Settings.secret.google_search_api.key,
+        cx: Settings.secret.google_search_api.cx,
+        q: search_word,
+        num: 1,
+        start: rand(1..10),
+        searchType: "image",
+      }
+    )
   end
 end
