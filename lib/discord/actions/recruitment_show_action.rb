d4877eef5e140749cb4b737de9dc741b38f56b3d
utyosu <utyosu@gmail.com>
2020-02-12 23:54:56 +0900

    PrivateMessageで案件が出せない不具合の修正

--- lib/discord/actions/recruitment_show_action.rb
+++ lib/discord/actions/recruitment_show_action.rb
@@ -4,7 +4,8 @@ class RecruitmentShowAction < RecruitmentBase
   end
 
   def execute?(message_event)
-    message_event.recruitment? && message_event.match_any_keywords?(Settings.keyword.recruitment.show)
+    (message_event.recruitment? || message_event.pm?) &&
+      message_event.match_any_keywords?(Settings.keyword.recruitment.show)
   end
 
   # respondable is MessageEvent or Channel object
