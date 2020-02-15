40750323fe6406668e6896f34937d4e5a99b4901
utyosu <utyosu@gmail.com>
2020-02-15 23:38:56 +0900

    rubocop-performacneによる指摘箇所の修正

--- lib/discord/actions/interaction_create_action.rb
+++ lib/discord/actions/interaction_create_action.rb
@@ -11,7 +11,7 @@ class InteractionCreateAction
     Activity.add(message_event.author, :interaction_create)
 
     _command, keyword, response = Extractor.format(message_event.content).split(/ /, 3)
-    return if keyword.blank? || !(1..64).include?(keyword.size) || response.blank? || !(1..64).include?(response.size)
+    return if keyword.blank? || !(1..64).cover?(keyword.size) || response.blank? || !(1..64).cover?(response.size)
     user = User.get_by_discord_user(message_event.author)
     Interaction.create(user: user, keyword: keyword, response: response)
     message_event.send_message(I18n.t("interaction.remember", keyword: keyword, response: response))
